18d6ad1b8f05394a7347f629c7b5a43801c27b8d
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-06-25 17:40:22 +0000

    automatically backed up by learn

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,13 +1,11 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-def display_board
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-end
+board = ["X", " ", " ", " ", " ", "O ", " ", " ", " "]
 
 display_board
-display_board 
-display_board
+puts " #{board[0]}  |#{board[1]}   | #{board[2]}  "
+puts "-----------"
+puts "  #{board[3]} | #{board[4]}  | #{board[5]}  "
+puts "-----------"
+puts "  #{board[6]} | #{board[7]}  | #{board[7]}  "
+
+
+board[1] = "X"
