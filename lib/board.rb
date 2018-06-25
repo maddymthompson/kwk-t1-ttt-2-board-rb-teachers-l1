7f0e099105c2556bc780bcd1baf60a2e435b2b1b
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-06-25 17:44:19 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
 board = ["X", " ", " ", " ", " ", "O ", " ", " ", " "]
 
 def display_board
+  board = ["X", " ", " ", " ", " ", "O ", " ", " ", " "]
   puts " #{board[0]}  |#{board[1]}  | #{board[2]}  "
   puts "-----------"
   puts " #{board[3]} | #{board[4]}  | #{board[5]}  "
@@ -9,5 +10,3 @@ def display_board
 end
 
 display_board
-
-board[1] = "X"
